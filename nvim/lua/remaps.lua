00c8a3a13e97b85afe20738f17db51aca0b06b0e
a.vladimirov <archibaldvladimirov@gmail.com>
2025-01-05 20:27:29 +0300

    system buffer yank

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 -- move selected
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- copy into system buffer
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
