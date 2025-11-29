371bceae2cd3aaea90da321df1fdeedee83dd80b
Ark Vladimirov <arqa39@172.28.98.227-red.dhcp.yndx.net>
2025-11-29 22:50:14 +0300

    arcsigns.nvim

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -1,4 +1,17 @@
+local arcadia = vim.fn.expand(vim.env.ARCADIA_ROOT or "~/arcadia")
+local function within_arcadia()
+    return string.find(vim.fn.getcwd(), arcadia) ~= nil
+end
+
 return {
-	"lewis6991/gitsigns.nvim",
- 	opts = {}
+	{
+		"lewis6991/gitsigns.nvim",
+		cond = function() return not within_arcadia() end,
+        config = function() require("gitsigns").setup{} end,
+	},
+	{
+		dir = arcadia .. "/contrib/tier1/gitsigns.arc.nvim",
+		cond = function() return within_arcadia() end,
+        config = function() require("gitsigns").setup{} end,
+	},
 }
