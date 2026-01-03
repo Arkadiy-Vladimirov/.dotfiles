a238accd81ef111216a65ea85b9f8708f81dccc0
Ark Vladimirov <arqa39@MacBook-Pro.local>
2026-01-03 16:21:22 +0300

    fish update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,8 @@
 # Use Vi mode
-fish_vi_key_bindings
+set --global fish_key_bindings fish_vi_key_bindings
 
 # Bind Ctrl-E to perform completion
 bind -M insert \ce end-of-line
+
+# Add local binaries to fish path
+fish_add_path -m ~/.local/bin
